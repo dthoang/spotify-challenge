875468e8249a823f18c2cad070b953b0f65ed7b1
dthoang <dthoang@uw.edu>
2015-11-10 03:26:01 -0800

    unfinished work

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
 var data;
+var artistData;
 var baseUrl = 'https://api.spotify.com/v1/search?type=track&query=';
-var topTracks
+var baseArtist = 'https://api.spotify.com/v1/search?type=track&query='
 
 var myApp = angular.module('myApp', [])
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
 
+  //Searches songs by the name of the track
   $scope.getTracks = function() {
     $http.get(baseUrl + $scope.track).success(function(response){
       data = $scope.tracks = response.tracks.items
     })
   }
+
+  // $scope.getArtist = function() {
+  //   console.log("hi")
+  //   $http.get(baseArtist + $scope.artist).success(function(response){
+  //     console.log("ho")
+  //     artistData = $scope.artists = response.artists.items
+  //     console.log(artistData)
+  //   }) 
+  // }
 
   //Plays a sample of the song found on Spotify
   $scope.play = function(song) {
